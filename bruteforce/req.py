752ff0852361a2da895b28408c0f171d7107eeb5
Lucas <lucas.santini@nddigital.com.br>
2020-04-28 14:26:39 -0300

    modificações no cabeçalho através de Post

--- bruteforce/req.py
+++ bruteforce/req.py
@@ -1,12 +1,14 @@
 # https://www.youtube.com/watch?v=2Co4cjPMTnk&list=PLp95aw034Wn_WtEmlepaDrw8FU8R5azcm&index=12
-
+# https://putsreq.com/4065hdVOGOpcJvOaUIRx
 import requests
 
 
-cabecalho = {'User-agent': 'Windows 12'}
+cabecalho = {'User-agent': 'Windows 12',
+             'Referer': 'http://www.siteinexistente.com.br'}
 texto = None
 try:
-    requisicao = requests.post('http://www.g1.com.br/', headers=cabecalho)
+    requisicao = requests.post('https://putsreq.com/uiIrN58xcNLQNpoTH9At',
+                               headers=cabecalho)
     texto = requisicao.text
 except Exception as e:
     print("requisição com erro:", e)
