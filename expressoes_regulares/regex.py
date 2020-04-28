b17465d04b576c5abb41e241366c90da790d416c
Lucas <lucas.santini@nddigital.com.br>
2020-04-28 18:07:53 -0300

    envio dos arquivos

--- expressoes_regulares/regex.py
+++ expressoes_regulares/regex.py
@@ -1,2 +1,19 @@
 import re
 
+string_teste = 'o rato roeu a roupa do rei de roma'
+
+# padrao = re.search(r'rato', string_teste)
+padrao = re.search(r'rat.', string_teste)
+# padrao = re.search(r'rat\w', string_teste)
+# padrao = re.search(r'ra\w\w', string_teste)
+padrao_f = re.findall(r'ro\w*', string_teste)
+
+if padrao:
+    print(padrao.group())
+else:
+    print('Padrão não encontrado')
+
+if padrao_f:
+    print(padrao_f)
+else:
+    print('Padrão não encontrado')
