ad8f2baeeb7a20c3cafe405c64d0f99d135dbcc9
Lucas <lucas.santini@nddigital.com.br>
2020-04-28 15:37:58 -0300

    API Funcionando com adaptações Adicionado API Key que o site exige, pois o curso foi feito antes disto ser implementado

--- API_filmes/main.py
+++ API_filmes/main.py
@@ -1,1 +1,36 @@
 # teste API
+import json
+
+import requests
+
+
+def requisicao(titulo):
+    try:
+        req = requests.get('http://www.omdbapi.com/?apikey=26f9b1c0&t=' + titulo)
+        dicionario = json.loads(req.text)
+        return dicionario
+    except:
+        print('erro de conexão')
+        return None
+
+
+def print_detalhes(filme):
+    print(filme['Title'])
+    print(filme['Actors'])
+    print(filme['Year'])
+    print(filme['Plot'])
+
+
+sair = False
+
+while not sair:
+    op = input('Digite o nome do filme ou [sair] para finalizar: ')
+    if op == 'sair':
+        sair = True
+        print('Saiu do sistema')
+    else:
+        filme = requisicao(op)
+        if filme['Response'] == 'False':
+            print('Filme não encontrado')
+        else:
+            print_detalhes(filme)
